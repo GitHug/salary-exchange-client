94da016f087e22a6f6d585669008c9952ab8047e
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-04-05 23:36:05 +0100

    Add info component

---
+++ src/components/Info.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <p>
+    Do you get your salary in another currency than the country you are currently living?
+
+    Or are you perhaps planning a trip and want to get an indication when it might be a
+    good time to exchange your currency?
+
+    Then this is the tool for you!
+
+    Add the currency you currently have to the left and the currency you want to convert
+    to the right.
+
+    Then just specify the amount and, presto, a graph showing the exchange rate for the
+    specified period.
+    </p>
+  </div>
+);
