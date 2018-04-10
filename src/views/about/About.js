e3fbb7d5f6719af45ad980e2f47ba525e98a0e6a
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-04-10 20:36:35 +0100

    Add white background to about page

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -4,34 +4,35 @@ import './styles/About.css';
 
 export default () => (
   <div className="About">
-    <h1>What is this thing?</h1>
+    <section className="text">
+      <h1>What is this thing?</h1>
 
-    <p>As someone who is currently living abroad, but still get my salary paid
+      <p>As someone who is currently living abroad, but still get my salary paid
       in the currency of my home country, I often wonder how much my buying power
       actually is. The exchange rate is quite important As I will be spending my
       salary in the country I currently live in.
-    </p>
+      </p>
 
-    <p>The exchange rate comes from the European Central Bank. The rate is updated
+      <p>The exchange rate comes from the European Central Bank. The rate is updated
       once every bank day at around 16:00 GMT.
-    </p>
+      </p>
 
-    <p> I created this tool partly for me to keep track of my buying power, but I hope
+      <p> I created this tool partly for me to keep track of my buying power, but I hope
       that you will find it useful as well.
-    </p>
+      </p>
 
-    <p>The code is open source and available on&#160;
-      <a
-        href="https://github.com/GitHug/salary-exchange-client"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <p>The code is open source and available on&#160;
+        <a
+          href="https://github.com/GitHug/salary-exchange-client"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
         GitHub<img src="GitHub-Mark-64px.png" alt="Github repo" />
-      </a>
+        </a>
         .
-    </p>
+      </p>
 
-    <Link to="/" className="back">Back to home</Link>
-
+      <Link to="/" className="back">Back to home</Link>
+    </section>
   </div>
 );
