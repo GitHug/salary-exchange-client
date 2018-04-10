4267693fa8badbc6ec0314b6db0f4f064fc225c3
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-04-10 21:18:37 +0100

    Fix NotFound page

--- src/views/notfound/NotFound.js
+++ src/views/notfound/NotFound.js
@@ -1,3 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../about/styles/About.css';
 
-export default () => <span>404</span>;
+export default () => (
+  <div className="NotFound">
+    <section className="text">
+      <h1>Where are you going aka 404?</h1>
+      <p>
+        I have no idea where you are heading, but there is nothing here.
+      </p>
+
+      <Link to="/" className="back">Take me back to safety</Link>
+    </section>
+  </div>
+);
